5247eb1753ba9cada7e2d0c03686f807808a90a5
Alex Graveley <alex@beatniksoftware.com>
2007-09-06 22:32:04 +0000

    Merge salma-hayek changes 2347:2356.

--- src/native/all/java-launcher/WindowsDirectoryChange.h
+++ src/native/all/java-launcher/WindowsDirectoryChange.h
@@ -13,3 +13,18 @@ public:
 };
 
 #endif
+#ifndef WINDOWS_DIRECTORY_CHANGE_H_included
+#define WINDOWS_DIRECTORY_CHANGE_H_included
+
+#include <string>
+
+struct WindowsDirectoryChange {
+private:
+    std::string previousDirectory;
+    
+public:
+    WindowsDirectoryChange(const std::string& targetDirectory);
+    ~WindowsDirectoryChange();
+};
+
+#endif
--- src/native/all/java-launcher/synchronizeWindowsEnvironment.h
+++ src/native/all/java-launcher/synchronizeWindowsEnvironment.h
@@ -4,3 +4,9 @@
 void synchronizeWindowsEnvironment();
 
 #endif // synchronizeWindowsEnvironment_h_
+#ifndef synchronizeWindowsEnvironment_h_
+#define synchronizeWindowsEnvironment_h_
+
+void synchronizeWindowsEnvironment();
+
+#endif // synchronizeWindowsEnvironment_h_

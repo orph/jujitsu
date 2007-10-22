0d5b788d918f4203134a72b3bfe74fe507ffab5f
Alex Graveley <alex@beatniksoftware.com>
2007-10-22 06:55:25 +0000

    Merge salma-hayek changes 2404:2406.

--- src/e/tools/HtmlViewer.java
+++ src/e/tools/HtmlViewer.java
@@ -4,55 +4,21 @@ import e.gui.*;
 import e.util.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.text.html.*;
 
 /**
- * A stand-alone program to render an HTML file in a window. This is sometimes
- * a convenient thing to have, less overkill than IE or Mozilla, and less
- * round-about than mailing it to yourself to view in Outlook.
+ * A stand-alone program to render an HTML file in a window.
+ * This is sometimes a convenient thing to have, less overkill than IE or Mozilla, and less round-about than mailing it to yourself to view in Outlook.
  */
 public class HtmlViewer extends MainFrame {
-    private JTextPane textPane;
+    private HtmlPane htmlPane;
     
     public HtmlViewer(String text) {
         Log.setApplicationName("HtmlViewer");
-        initTextPane();
-        setText(text);
+        htmlPane = new HtmlPane();
+        htmlPane.setText(text);
         setTitle("HTML Viewer");
-        getContentPane().add(new JScrollPane(textPane));
+        getContentPane().add(new JScrollPane(htmlPane));
         setSize(new Dimension(640, 480));
-    }
-    
-    public void initTextPane() {
-        textPane = new JTextPane();
-        textPane.setEditable(false);
-        textPane.setContentType("text/html");
-        
-        textPane.addHyperlinkListener(new HyperlinkListener() {
-            public void hyperlinkUpdate(HyperlinkEvent e) {
-                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                    if (e instanceof HTMLFrameHyperlinkEvent) {
-                        ((HTMLDocument) textPane.getDocument()).processHTMLFrameHyperlinkEvent((HTMLFrameHyperlinkEvent) e);
-                    } else {
-                        Log.warn("can't yet follow links out of the document");
-                        System.exit(0);
-                        //Edit.showDocument(e.getURL().toString());
-                    }
-                }
-            }
-        });
-        
-        HTMLEditorKit editorKit = (HTMLEditorKit) textPane.getEditorKit();
-        StyleSheet styleSheet = editorKit.getStyleSheet();
-        styleSheet.removeStyle("body");
-        styleSheet.addRule("body { font-family: Arial, Helvetica, sans-serif }");
-        styleSheet.addRule("body { font-size: 10 }");
-    }
-    
-    public void setText(String text) {
-        textPane.setText(text);
-        textPane.setCaretPosition(0);
     }
     
     public static void main(String[] arguments) {
